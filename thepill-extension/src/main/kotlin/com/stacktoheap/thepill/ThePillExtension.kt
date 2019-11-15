5fbc651d62a9519d55a713eb78582afd39c6a794
Manoj <manojlds@gmail.com>
2019-11-15 14:53:02 +0530

    Decision id support in unmanaged extension

--- thepill-extension/src/main/kotlin/com/stacktoheap/thepill/ThePillExtension.kt
+++ thepill-extension/src/main/kotlin/com/stacktoheap/thepill/ThePillExtension.kt
@@ -27,9 +27,14 @@ class ThePillExtension(@Context val graphDb: GraphDatabaseService) {
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
     @Path("/next_step/{decisionName}")
-    fun next_step(@PathParam("decisionName") decisionName: String, body: String): Response {
-        val queryArguments = MapUtil.map("decisionName", decisionName, "facts", parseMap(body))
-        val result = graphDb.execute(STEP_QUERY, queryArguments)
+    fun next_step(@PathParam("decisionName") decisionName: String, @QueryParam("decisionId") decisionId: Long?, body: String): Response {
+        val (query, queryArguments) =
+            when(decisionId) {
+                null -> Pair(STEP_QUERY, MapUtil.map("decisionName", decisionName, "facts", parseMap(body)))
+                else -> Pair(STEP_QUERY_WITH_ID, MapUtil.map("decisionName", decisionName, "facts", parseMap(body), "decisionId", decisionId))
+            }
+
+        val result = graphDb.execute(query, queryArguments)
         val response = result.asSequence().map { it.mapValues { it.value } }.toList()
 
         return asResponse(response)
@@ -39,6 +44,7 @@ class ThePillExtension(@Context val graphDb: GraphDatabaseService) {
         val OBJECT_MAPPER: ObjectMapper = ObjectMapper()
         val DECISION_QUERY = "CALL com.stacktoheap.thepill.make_decision(\$decisionName, \$facts, true) yield path return last(nodes(path)).value as result"
         val STEP_QUERY = "CALL com.stacktoheap.thepill.next_step(\$decisionName, \$facts) yield result return result as stepResult"
+        val STEP_QUERY_WITH_ID = "CALL com.stacktoheap.thepill.next_step(\$decisionName, \$facts, \$decisionId) yield result return result as stepResult"
     }
 
     fun asResponse(result: List<Map<String, Any?>>) = Response.ok().entity(formatMap(result)).build()
--- thepill-extension/src/test/kotlin/com/stacktoheap/thepill/ThePillExtensionTest.kt
+++ thepill-extension/src/test/kotlin/com/stacktoheap/thepill/ThePillExtensionTest.kt
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
+import org.neo4j.driver.internal.value.MapValue
 import org.neo4j.driver.v1.GraphDatabase
 import org.neo4j.harness.ServerControls
 import org.neo4j.harness.TestServerBuilders
@@ -32,8 +33,6 @@ class ThePillExtensionTest {
         }
     }
 
-    data class Result(val result: String)
-
     @Test
     fun `test decision tree traversal`() {
         GraphDatabase.driver(embeddedDatabaseServer.boltURI()).use { driver ->
@@ -51,7 +50,7 @@ class ThePillExtensionTest {
 
                 )
 
-                val response = HTTP.POST(embeddedDatabaseServer.httpURI().resolve("/thepill/make_decision/neo").toString(), "{}")
+                val response = HTTP.POST(embeddedDatabaseServer.httpURI().resolve("/thepill/make_decision/neo").toString(), mapOf<String, String>())
                 assertTrue(response.status() == 200)
                 val result = response.content<List<Map<String, String>>>()
                 assertThat(result.flatMap { it.values }).containsAll(listOf("knowledge", "ignorance"))
@@ -75,13 +74,55 @@ class ThePillExtensionTest {
 
                 )
 
-                val response = HTTP.POST(embeddedDatabaseServer.httpURI().resolve("/thepill/next_step/neo").toString(), "{}")
+                val response = HTTP.POST(embeddedDatabaseServer.httpURI().resolve("/thepill/next_step/neo").toString(), mapOf<String, String>())
+                assertTrue(response.status() == 200)
+
+                val result = response.content<List<Map<String, Map<String, String>>>>()
+                assertThat(result.size).isEqualTo(1)
+
+                val decisionResult = result[0]["stepResult"]
+                assertThat(decisionResult!!["question"]).isEqualTo("Red Pill Or Blue Pill")
+            }
+        }
+    }
+
+    @Test
+    fun `test decision step traversal with id`() {
+        GraphDatabase.driver(embeddedDatabaseServer.boltURI()).use { driver ->
+            driver.session().use { session ->
+                session.run(
+                    "" +
+                            "CREATE (tree:Tree { name: 'neo' })" +
+                            "CREATE (ready: Decision { name: 'are_you_ready', parameters:['ready'], question: 'Are you ready?', choice: 'result = {relationship: \"YES\"}; if(!ready) result = {relationship: \"NO\"};' })" +
+                            "CREATE (pill1: Decision { name: 'pill_decision', parameters:['chosenColor'], question: 'Red Pill Or Blue Pill?', choice: 'result = {relationship: \"RED\"};' })" +
+                            "CREATE (pill2: Decision { name: 'pill_decision', parameters:['chosenColor'], question: 'Blue Pill or Red Pill?', choice: 'result = {relationship: \"BLUE\"};' })" +
+                            "CREATE (red:Leaf { value: 'knowledge' })" +
+                            "CREATE (blue:Leaf { value: 'ignorance' })" +
+                            "CREATE (tree)-[:HAS]->(ready)" +
+                            "CREATE (ready)-[:YES]->(pill1)" +
+                            "CREATE (ready)-[:NO]->(pill2)" +
+                            "CREATE (pill1)-[:RED]->(red)" +
+                            "CREATE (pill2)-[:RED]->(red)" +
+                            "CREATE (pill1)-[:BLUE]->(blue)" +
+                            "CREATE (pill2)-[:BLUE]->(blue)"
+
+                )
+
+                val resultFromReadyDecision = session.run("CALL com.stacktoheap.thepill.next_step('are_you_ready', {ready: true}) yield result return result")
+                    .single().get(0) as MapValue
+
+                val nextDecisionId = resultFromReadyDecision.get("decisionId").asLong()
+                assertTrue(nextDecisionId > 0)
+                val nextDecisionName = resultFromReadyDecision.get("name").asString()
+
+                val response = HTTP.POST(embeddedDatabaseServer.httpURI().resolve("/thepill/next_step/$nextDecisionName?decisionId=$nextDecisionId").toString(), mapOf("chosenColor" to "red"))
                 assertTrue(response.status() == 200)
                 val result = response.content<List<Map<String, Map<String, String>>>>()
 
+                assertThat(result.size).isEqualTo(1)
                 val decisionResult = result[0]["stepResult"]
 
-                assertThat(decisionResult!!["question"] == "Red Pill Or Blue Pill")
+                assertThat(decisionResult!!["value"]).isEqualTo("knowledge")
 
 
             }
